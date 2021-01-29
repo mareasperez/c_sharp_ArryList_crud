125048bf86ab2771d1ee147c60f9d0f09c77d126
Maury <mareasperez7@gmail.com>
2021-01-28 22:51:20 -0600

    Agregar archivos de proyecto.

---
+++ Lista/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsMostarArrayList Muestra = new clsMostarArrayList();
+            //Muestra.Capturadatos();
+            //Muestra.ImprimeDatos();
+            //Muestra.BuscaDato();
+            //Muestra.EliminaDato();
+            bool continuar = true;
+            do
+            {
+                Console.WriteLine("-----MENU------");
+                Console.WriteLine("1) Ingresar Nombres.");
+                Console.WriteLine("2) Imprimir Nombres.");
+                Console.WriteLine("1) Buscar Nombre.");
+                Console.WriteLine("3) Eliminar Nombre.");
+                //seleccion = Console.ReadKey();
+            } while (continuar);
+
+        }
+    }
+}
---
+++ Lista/clsMostarArrayList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    public class clsMostarArrayList
+    {
+        ArrayList Lista;
+        public clsMostarArrayList()
+        {
+            Lista = new ArrayList();
+        }
+        public void Capturadatos()
+        {
+            string cadena;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Ingrese el elemento {0}: ", i + 1);
+                cadena = Console.ReadLine();
+                Lista.Add(cadena);
+            }
+        }
+        public void BuscaDato()
+        {
+            string busqueda;
+            Console.WriteLine("Ingrese el Nombre a buscar");
+            busqueda = Console.ReadLine();
+            if (Lista.Contains(busqueda))
+            {
+                Console.WriteLine("El Nombre: " + busqueda + " se encontro en la lista");
+            }
+            else
+            {
+                Console.WriteLine("El Nombre: " + busqueda + " no se encontro en la lista");
+            }
+            Console.ReadKey();
+        }
+
+        public void ImprimeDatos()
+        {
+            Console.WriteLine("Imprimiendo Datos de la lista");
+            foreach (string item in Lista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        public void EliminaDato()
+        {
+            string busqueda;
+            Console.WriteLine("Ingrese el Nombre a eliminar");
+            busqueda = Console.ReadLine();
+            if (Lista.Contains(busqueda))
+            {
+                Lista.Remove(busqueda);
+                Console.WriteLine("Luego de la eliminacion la lista quedo de la siguiente manera:");
+                ImprimeDatos();
+            }
+            else
+            {
+                Console.WriteLine("El Nombre: " + busqueda + " no se encontro en la lista");
+                Console.WriteLine("La lista de elementos disponibles es la liguiente:");
+                ImprimeDatos();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
